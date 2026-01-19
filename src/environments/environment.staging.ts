d2b87bf045c5c90fa1d32f64791a37176107ab73
emmanuel <emmanuelpondi07@gmail.com>
2026-01-19 13:16:02 +0100

    feat: Add staging environment configuration for deployment

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: true,
+  production: false,
   apiUrl: 'https://infotech-ss.onrender.com/api',
   stripePublicKey: 'pk_test_51SqFLG3aW6k300kibdUo83fjTjMrOZUp1lxuFpHjjTYAiypf4sDhCPWjFCxRu2aYq6VtjnkKei2eK1HX0Bp5MW3O00rP4dvNAR'
 };
