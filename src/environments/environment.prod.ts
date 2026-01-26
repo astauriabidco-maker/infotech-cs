ce2ac55f344266664fec74a004c0d57e9601c941
emmanuel <emmanuelpondi07@gmail.com>
2026-01-26 16:07:18 +0100

    fix: Update backend URL to Railway production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://infotech-ss.onrender.com/api',
+  apiUrl: 'https://infotech-ss-production.up.railway.app/api',
   stripePublicKey: 'pk_test_51SqFLG3aW6k300kibdUo83fjTjMrOZUp1lxuFpHjjTYAiypf4sDhCPWjFCxRu2aYq6VtjnkKei2eK1HX0Bp5MW3O00rP4dvNAR'
 };
